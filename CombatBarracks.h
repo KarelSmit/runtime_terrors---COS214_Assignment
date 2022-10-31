d9e85b1cdc0d466a64cb95f65f67caae4aaa3c4e
emileepeyton <u21494577@tuks.co.za>
2022-10-31 02:06:19 +0200

    HeaderFiles for AbstractFactory

---
+++ CombatBarracks.h
@@ -0,0 +1,9 @@
+#ifndef COMBATBARRACKS_H
+#define COMBATBARRACKS_H
+#include "Barracks.h"
+class CombatBarracks : public Barracks{ 
+    public: 
+    void concreteProductA(); 
+    void concreteProductB(); 
+}; 
+#endif
---
+++ Infrantry.h
@@ -0,0 +1,7 @@
+#ifndef INFANTRY_H
+#define INFANTRY_H
+#include "FootUnit.h"
+class Infantry : public FootUnit { 
+
+};
+#endif
