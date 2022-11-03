4d26aef483746737721f4aa5bfd572204bed524d
emileepeyton <u21494577@tuks.co.za>
2022-11-03 16:42:17 +0200

    Vehicle .h updated

--- Vehicle.h
+++ Vehicle.h
@@ -1,11 +1,9 @@
 #ifndef VEHICLE_H
 #define VEHICLE_H
 #include <string>
-#include "TransportUnit.h"
 class Vehicle {
     public: 
     Vehicle();
-    TransportUnit* driver; 
     virtual ~Vehicle();
 };
 #endif
