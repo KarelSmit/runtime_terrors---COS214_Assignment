9c2ed4dab20ed6b3be0c037a3564047a368c1b7f
Matthew <104757529+matthewels@users.noreply.github.com>
2022-11-04 14:45:48 +0200

    tank update

--- Tank.cpp
+++ Tank.cpp
@@ -3,6 +3,31 @@ Tank::Tank(){
 
 }
 
+Tank::Tank(string name) {
+    name = "T-" + name;
+    this->name = name;
+}
+
 Tank::~Tank() {
     
+}
+
+void Tank::print() {
+    cout<<name<<endl;
+}
+
+Tank& Tank::operator=(const Tank& m) {
+    this->name = m.name;
+    return *this;
+}
+bool Tank::operator==(const Tank& m) {
+    if  (this->name == m.name)
+        return true;
+    else
+        return false;
+}
+
+std::ostream& operator<<(std::ostream& out, const Tank& m){
+    out << m.name;
+    return out;
 }
