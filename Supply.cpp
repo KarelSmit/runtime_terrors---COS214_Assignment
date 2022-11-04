9c88fd3c18f6ab3406d31afb3ff57899f994b10b
Matthew <104757529+matthewels@users.noreply.github.com>
2022-11-04 15:25:13 +0200

    Supply update

--- Supply.cpp
+++ Supply.cpp
@@ -1,6 +1,6 @@
 #include "Supply.h"
 #include "SupplyTruckFactory.h"
-Supply::Supply(int HP) : TransportUnit(HP) {
+Supply::Supply(int HP,string name) : TransportUnit(HP) {
     SupplyTruckFactory* newVehicle = new SupplyTruckFactory(); 
     supplyTruck = newVehicle->createVehicles(); 
 }
@@ -11,4 +11,25 @@ Supply::~Supply() {
 
 Vehicle* getSupplyTruck() {
     return this->supplyTruck;
+}
+
+void Supply::print() {
+    cout<<name<< "(" << HP <<")"<<endl;
+}
+
+Supply& Supply::operator=(const Supply& m) {
+    this->name = m.name;
+    this ->HP = m.HP;
+    return *this;
+}
+bool Supply::operator==(const Supply& m) {
+    if  ((this->name == m.name)&&this->HP == m.HP)
+        return true;
+    else
+        return false;
+}
+
+std::ostream& operator<<(std::ostream& out, const Medic& m){
+    out << m.name<< "(" << m.HP <<")";
+    return out;
 }
