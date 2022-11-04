2fc1a44e3736cb625093bf00a8603bc0ebf30d18
Matthew <104757529+matthewels@users.noreply.github.com>
2022-11-04 09:15:59 +0200

    added function to medic

--- Medic.cpp
+++ Medic.cpp
@@ -10,3 +10,25 @@ Medic::~Medic(){
 int Medic::getRP() {
     this-> RP;
 }
+
+void Supply::print() {
+    cout<<name<< "(" << hp << ":"<< rp<<")"<<endl;
+}
+
+Supply& Supply::operator=(const Supply& m) {
+    this->rp = m.rp;
+    this->hp = m.hp;
+    this->name = m.name;
+    return *this;
+}
+bool Supply::operator==(const Supply& m) {
+    if  ((this->rp == m.rp) && (this->hp == m.hp) && (this->name == m.name))
+        return true;
+    else
+        return false;
+}
+
+std::ostream& operator<<(std::ostream& out, const Supply& m){
+    out << m.name<< "(" << m.hp << ":"<<m.rp<<")";
+    return out;
+}
