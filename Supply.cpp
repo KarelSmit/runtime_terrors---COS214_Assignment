0a85f0a04c87dc5fed19746e3aff27ec87407436
KarelSmit <u20447932@tuks.co.za>
2022-11-05 01:38:55 +0200

    format

--- Supply.cpp
+++ Supply.cpp
@@ -1,36 +1,43 @@
 #include "Supply.h"
 #include "SupplyTruckFactory.h"
 #include <string>
-Supply::Supply(int HP,string name) : TransportUnit(HP) {
-    SupplyTruckFactory* newVehicle = new SupplyTruckFactory(); 
-    supplyTruck = newVehicle->createVehicles(); 
+Supply::Supply(int HP, string name) : TransportUnit(HP)
+{
+	SupplyTruckFactory *newVehicle = new SupplyTruckFactory();
+	supplyTruck = newVehicle->createVehicles();
 }
 
-Supply::~Supply() { 
-    supplyTruck->~Vehicle();
+Supply::~Supply()
+{
+	supplyTruck->~Vehicle();
 }
 
-Vehicle* getSupplyTruck() {
-    return this->supplyTruck;
+Vehicle *getSupplyTruck()
+{
+	return this->supplyTruck;
 }
 
-void Supply::print() {
-    cout<<name<< "(" << HP <<")"<<endl;
+void Supply::print()
+{
+	cout << name << "(" << HP << ")" << endl;
 }
 
-Supply& Supply::operator=(const Supply& m) {
-    this->name = m.name;
-    this ->HP = m.HP;
-    return *this;
+Supply &Supply::operator=(const Supply &m)
+{
+	this->name = m.name;
+	this->HP = m.HP;
+	return *this;
 }
-bool Supply::operator==(const Supply& m) {
-    if  ((this->name == m.name)&&this->HP == m.HP)
-        return true;
-    else
-        return false;
+bool Supply::operator==(const Supply &m)
+{
+	if ((this->name == m.name) && this->HP == m.HP)
+		return true;
+	else
+		return false;
 }
 
-std::ostream& operator<<(std::ostream& out, const Medic& m){
-    out << m.name<< "(" << m.HP <<")";
-    return out;
+std::ostream &operator<<(std::ostream &out, const Medic &m)
+{
+	out << m.name << "(" << m.HP << ")";
+	return out;
 }
