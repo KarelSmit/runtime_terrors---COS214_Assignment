45253f62120ffbc2d822e3b58e5b39dc5e3e5b01
Matthew <104757529+matthewels@users.noreply.github.com>
2022-11-04 14:45:17 +0200

    tank header update

--- Tank.h
+++ Tank.h
@@ -1,9 +1,20 @@
 #ifndef TANK_H
 #define TANK_H
 #include "Vehicle.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 class Tank : public Vehicle {
-    public: 
+    public:
+    string name;
     Tank();
-    virtual ~Tank(); 
+    Tank(string name);
+    virtual ~Tank();
+    void print();
+    Tank& operator=(const Tank& m);
+    bool operator==(const Tank& m);
+    friend std::ostream& operator<<(std::ostream& out, const Tank& m);
 };
 #endif
