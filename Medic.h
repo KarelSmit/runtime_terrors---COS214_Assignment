09a9e980ddbb7e40d3e4da845695fa23c59a989f
Matthew <104757529+matthewels@users.noreply.github.com>
2022-11-04 09:18:19 +0200

    medic header update

--- Medic.h
+++ Medic.h
@@ -1,6 +1,7 @@
 #ifndef MEDIC_H
 #define MEDIC_H
 #include "FootUnit.h"
+#include <iostream>
 class Medic : public FootUnit{
     private: 
     int RP; 
@@ -8,5 +9,10 @@ class Medic : public FootUnit{
     Medic(int, int); 
     virtual ~Medic();
     int getRP();
+    //  ***** need to include a =operator method *****, i%
+    Medic& operator=(const Medic& m);
+    bool operator==(const Medic& m);
+    friend std::ostream& operator<<(std::ostream& out, const Medic& m);
+
 };
 #endif
