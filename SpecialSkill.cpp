368fcce41e85b3765ba24e5878c67faa90a0000e
emileepeyton <u21494577@tuks.co.za>
2022-11-05 15:50:30 +0200

    decorator added

--- SpecialSkill.cpp
+++ SpecialSkill.cpp
@@ -1,4 +1,6 @@
 #include "SpecialSkill.h"
+#include "Sniper.h"
+#include "Stealth.h"
 SpecialSkill::SpecialSkill(Infantry* soldier, int choice) {
     this->soldier = soldier; 
     this->choice = choice; 
@@ -6,8 +8,14 @@ SpecialSkill::SpecialSkill(Infantry* soldier, int choice) {
 
 void SpecialSkill::addSkill() {
     if (choice == 1) {
-        Sniper decorator(soldier);
+        Sniper* decorator = new Sniper(soldier);
+        delete decorator;
     } else if (choice == 2) {
-        Stealth decorator(soldier); 
+        Stealth* decorator = new Stealth(soldier); 
+        delete decorator;
     }
+}
+
+SpecialSkill::~SpecialSkill() {
+    
 }
