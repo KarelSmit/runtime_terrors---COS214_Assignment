ace7bafce18b1978c9653b297c825cd04b966dac
Karel-Louis Smit <81307096+KarelSmit@users.noreply.github.com>
2022-11-05 13:41:08 +0200

    Delete CombatBarracks.h

--- CombatBarracks.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMBATBARRACKS_H
-#define COMBATBARRACKS_H
-#include "Barracks.h"
-class CombatBarracks : public Barracks{ 
-    public: 
-    void concreteProductA(); 
-    void concreteProductB(); 
-}; 
-#endif
