df26129ce8d24214172f764b0f0785c1fd43a0f2
KarelSmit <u20447932@tuks.co.za>
2022-11-06 16:55:12 +0200

    Add Strategy.h doxygen

--- Strategy.h
+++ Strategy.h
@@ -1,3 +1,13 @@
+/**
+ * @file Strategy.h
+ * @author Karel Smit
+ * @brief Abstract class for the Strategy pattern
+ * @version 0.1
+ * @date 2022-11-06
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 #ifndef STRATEGY_H
 #define STRATEGY_H
 #include "Army.h"
@@ -5,9 +15,23 @@
 class Strategy
 {
 public:
+	/**
+	 * @brief Construct a new Strategy object
+	 *
+	 */
 	Strategy();
+	/**
+	 * @brief Destroy the Strategy object
+	 *
+	 */
 	~Strategy();
-	virtual void battlePlan( Army*, Army* ) = 0;
+	/**
+	 * @brief Initiate the chosen ConcreteStrategy for Army a against Army b
+	 *
+	 * @param a
+	 * @param b
+	 */
+	virtual void battlePlan(Army *a, Army *b) = 0;
 };
 
 #endif
