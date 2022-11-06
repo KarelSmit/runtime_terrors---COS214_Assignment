0ddf055f6c128cf71904c6b16be28fc9dd20b4f0
u21515086 <u21515086@tuks.co.za>
2022-11-06 08:41:48 +0200

    adding Doxygen comments

--- IntelDivision.h
+++ IntelDivision.h
@@ -1,9 +1,25 @@
+/**
+ * @file IntelDivision.h
+ * @author Natalie Walsh
+ * @brief Memento class
+ * @version 0.1
+ * @date 2022-11-06
+ * 
+ * 
+ */
+ 
 #ifndef INTELDIVISION_H
 #define INTELDIVISION_H
 #include "User.h"
 #include <string>
 #include <iostream>
 using namespace std;
+
+/**
+ * @class IntelDivision
+ * 
+ * @tparam  
+ */
 
 class IntelDivision
 {
@@ -12,11 +28,45 @@ private:
     string date_;
 
 public:
+    /**
+    * @brief Constructor of the IntelDivision object
+    * 
+    */
     IntelDivision(User *u);
+
+    /**
+	 * @brief Return the user attribute
+	 * 
+	 * @return User
+	 */
     User *getUser();
+
+    /**
+	 * @brief Return the user details and current date
+	 * 
+	 * @return String
+	 */
     string GetName() const;
+
+    /**
+	 * @brief To be saved to textfile
+	 * 
+	 * @return string
+	 */
     string getDetails();
+
+    /**
+	 * @brief Provide feedback to user 
+	 * 
+	 * @return string 
+	 */
     string print();
+
+    /**
+	 * @brief Return the current date and time
+	 * 
+	 * @return string
+	 */
     string date() const;
 };
 #endif
