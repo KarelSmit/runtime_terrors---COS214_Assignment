0dc9b07e0157e9055eec87565b5da02f323830d3
KarelSmit <u20447932@tuks.co.za>
2022-11-06 16:47:05 +0200

    Added Attack.h doxygen comments

--- Attack.h
+++ Attack.h
@@ -1,3 +1,13 @@
+/**
+ * @file Attack.h
+ * @author Karel Smit
+ * @brief Implementation of the Attack-type strategy
+ * @version 0.1
+ * @date 2022-11-06
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 #ifndef ATTACK_H
 #define ATTACK_H
 #include "Strategy.h"
@@ -5,8 +15,22 @@
 class Attack : public Strategy
 {
 public:
+	/**
+	 * @brief Construct a new Attack object
+	 *
+	 */
 	Attack();
+	/**
+	 * @brief Destroy the Attack object
+	 * 
+	 */
 	~Attack();
-	void battlePlan( Army*, Army* );
+	/**
+	 * @brief Initiate and Attack of Army a against Army b
+	 * 
+	 * @param a 
+	 * @param b 
+	 */
+	void battlePlan(Army* a, Army* b);
 };
 #endif
