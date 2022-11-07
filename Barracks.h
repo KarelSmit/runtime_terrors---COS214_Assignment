d2dbae6ca55b7371647eec39d556a5a4c5993128
emileepeyton <u21494577@tuks.co.za>
2022-11-07 14:37:33 +0200

    barracks doxygen

--- Barracks.h
+++ Barracks.h
@@ -1,3 +1,9 @@
+/**
+ * @file Barracks.h
+ * @author Emilee da Cruz
+ * @brief Header file for Barracks class
+ * @date 2022-11-06
+ */
 #ifndef BARRACKS_H
 #define BARRACKS_H
 #include "FootUnit.h"
@@ -5,11 +11,28 @@
 #include <iostream> 
 #include <iomanip> 
 #include <string> 
+/**
+*@class Barracks
+*/
 class Barracks {
     public:
+    /**
+    *@brief constructs a barracks object
+    */
     Barracks(); 
+    /**
+    *@brief virtual function for creating a foot unit
+    *@return footUnit*
+    */
     virtual FootUnit* createFootUnit() = 0; 
+    /**
+    *@brief virtual function for creating a TransportUnit
+    *@return TransportUnit*
+    */
     virtual TransportUnit* createTransportUnit() = 0;
+    /**
+    *@brief destroys barracks object
+    */
     virtual ~Barracks();
 }; 
 #endif
