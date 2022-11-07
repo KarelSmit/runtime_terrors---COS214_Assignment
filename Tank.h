0f095cfeba9574efb82936e8e551044ddb24db47
emileepeyton <u21494577@tuks.co.za>
2022-11-07 02:32:45 +0200

    Tank.h doxygen

--- Tank.h
+++ Tank.h
@@ -1,9 +1,24 @@
+/**
+ * @file Tank.h
+ * @author Emilee da Cruz
+ * @brief Header file for Tank class which inherits from Vehicle class
+ * @date 2022-11-06
+ */
 #ifndef TANK_H
 #define TANK_H
 #include "Vehicle.h"
+/**
+*@class Tank
+*/
 class Tank : public Vehicle {
     public: 
+    /**
+    *@brief constructor 
+    */
     Tank();
+    /**
+    *@brief destructor for destroying Tanks
+    */
     virtual ~Tank(); 
 };
 #endif
