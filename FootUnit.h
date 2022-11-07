ce3e3b353b217ef5e2c2968277a30c7b5f1b2a8b
emileepeyton <u21494577@tuks.co.za>
2022-11-07 14:32:34 +0200

    foot unit member variable

--- FootUnit.h
+++ FootUnit.h
@@ -1,14 +1,38 @@
-
+/**
+ * @file FootUnit.h
+ * @author Emilee da Cruz
+ * @brief Header file for FootUnit class
+ * @date 2022-11-06
+ */
 #ifndef FOOTUNIT_H
 #define FOOTUNIT_H
 class FootUnit {
     protected: 
-    int HP; 
+    int HP; // member variable for the HP
     public:
-    FootUnit(int); 
+    /**
+    *@brief constructs a foot unit object
+    *@param int
+    */
+    FootUnit(int);
+    /**
+    *@brief destroys a footunit object
+    */ 
     ~FootUnit();
-    int getHP(); 
+    /**
+    *@brief getter for the HP member variable
+    *@return int
+    */
+    int getHP();
+    /**
+    *@brief getter for the damage member variable
+    *@return int
+    */ 
 	virtual int getDamage();
+    /**
+    *@brief getter for rejuvenation power member variable
+    *@return int
+    */
 	virtual int getrejuvenationPower();
 };
 #endif
