4e951fda76730299bc4f456fb24fda83ab43b1bf
emileepeyton <u21494577@tuks.co.za>
2022-11-07 14:10:22 +0200

    SupplyTruckFact doxygen

--- SupplyTruckFactory.h
+++ SupplyTruckFactory.h
@@ -1,10 +1,29 @@
+/**
+ * @file SupplyTruckFactory.h
+ * @author Emilee da Cruz
+ * @brief Header file for SupplyTruckFactory class which inherits from VehicleFactory class
+ * @date 2022-11-06
+ */
 #ifndef SUPPLYTRUCKFACTORY_H
 #define SUPPLYTRUCKFACTORY_H
 #include "VehicleFactory.h"
+/**
+*@class SupplyTruckFactory
+*/
 class SupplyTruckFactory : public VehicleFactory {
     public: 
+    /**
+    *@brief Constructor for SupplyTruckFactory object
+    */
     SupplyTruckFactory(); 
+    /**
+    *@brief Inherited function for creating supply trucks
+    *@return Vehicle*
+    */
     Vehicle* createVehicles(); 
+    /**
+    *@brief Destructor
+    */
     virtual ~SupplyTruckFactory();
 };
 #endif
