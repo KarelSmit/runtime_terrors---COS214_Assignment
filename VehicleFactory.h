97d1f4081776e3935e11a2b74cdd1b750187dc76
emileepeyton <u21494577@tuks.co.za>
2022-11-07 02:11:40 +0200

    Vehicle factory doxygen

--- VehicleFactory.h
+++ VehicleFactory.h
@@ -1,11 +1,30 @@
+/**
+ * @file VehicleFactory.h
+ * @author Emilee da Cruz
+ * @brief Abstract vehicle factory class
+ * @date 2022-11-06
+ */
 #ifndef VEHICLEFACTORY_H
 #define VEHICLEFACTORY_H
 #include "Tank.h"
 #include "SupplyTruck.h"
+/**
+*@class VehicleFactory
+*/
 class VehicleFactory {
     public: 
-    VehicleFactory(); 
+    /**
+    *@brief Vehicle factory default constructor
+    */
+    VehicleFactory();
+    /**
+    *@brief virtual function for creating vehicles
+    *@return Vehicle*
+    */ 
     virtual Vehicle* createVehicles() = 0; 
+    /**
+    *@brief Vehicle factory default destructor
+    */
     virtual ~VehicleFactory(); 
 };
 #endif
