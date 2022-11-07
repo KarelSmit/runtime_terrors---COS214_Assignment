c6e6f62767acfde17b5ffe8b599ee0a964e9728c
emileepeyton <u21494577@tuks.co.za>
2022-11-07 14:22:35 +0200

    sniper doxygen

--- Sniper.h
+++ Sniper.h
@@ -1,9 +1,24 @@
+/**
+ * @file Tank.h
+ * @author Emilee da Cruz
+ * @brief Header file for Sniper class
+ * @date 2022-11-06
+ */
 #ifndef SNIPER_H
 #define SNIPER_H
 #include "Infantry.h"
+/**
+*@class Sniper
+*/
 class Sniper { 
     public: 
+    /**
+    *@brief constructor for sniper class
+    */
     Sniper(Infantry*); 
+    /**
+    *@brief destroys sniper object
+    */
     ~Sniper(); 
 }; 
 #endif 
