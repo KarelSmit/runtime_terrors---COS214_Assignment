50581efbe8b2cc8c0c52243f1cadc12dadb07653
KarelSmit <u20447932@tuks.co.za>
2022-11-07 15:37:18 +0200

    syntax fix

--- PreBattle.cpp
+++ PreBattle.cpp
@@ -13,7 +13,7 @@ void PreBattle::handle()
 {
 	context->getB()->setEnemy();
 	int totalBattalion = 6;
-	cout << "================================================================================================\n";
+	std::cout << "================================================================================================\n";
 	std::cout << "Your army will consist of " << totalBattalion << " battalions.\n\n";
 	std::cout << "Choose one of the combinations below:\n\n";
 	bool accept = false;
@@ -22,7 +22,7 @@ void PreBattle::handle()
 		std::cout << "1. 2x Infantry, 2x Tank, 2x Medic\n2. 2x Infantry, 2x Tank, 1x Medic, 1x Supply\n";
 		std::cout << "3. 1x Infantry, 2x Tank, 2x Medic, 1x Supply\n4. 2x Infantry, 1x Tank, 2x Medic, 1x Supply\n";
 		std::cout << "5. 1x Infantry, 1x Tank, 2x Medic, 2x Supply\n->";
-		string input;
+		std::string input;
 		std::cin >> input;
 		if (input == "1" || input == "2" || input == "3" || input == "4" || input == "5")
 		{
@@ -67,9 +67,9 @@ void PreBattle::handle()
 		}
 		else
 		{
-			cout << "================================================================================================\n";
+			std::cout << "================================================================================================\n";
 			std::cout << "\nInvalid input. Please select one of the given battalion types.\n\n";
-			cout << "================================================================================================\n";
+			std::cout << "================================================================================================\n";
 			accept = false;
 		}
 	}
