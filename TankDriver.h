bee2a1e5bc5b6cfd19b74278ecc003ef1ff4b970
emileepeyton <u21494577@tuks.co.za>
2022-11-07 02:29:31 +0200

    TankDriver doxygen

--- TankDriver.h
+++ TankDriver.h
@@ -1,16 +1,35 @@
+/**
+ * @file TankDriver.h
+ * @author Emilee da Cruz
+ * @brief Inherits from the transport unit class and created TankDriver objects
+ * @date 2022-11-06
+ */
 #ifndef TANKDRIVER_H
 #define TANKDRIVER_H
 #include "TransportUnit.h"
 #include "Vehicle.h"
+/**
+*@class TankDriver
+*/
 class TankDriver : public TransportUnit
 {
 private:
-	int Damage;
+	int Damage; // damage inflicted by a TankDriver
 
 public:
+/**
+*@brief constructor for tank driver. takes in two integers - one for HP and one for Damage
+*/
 	TankDriver(int, int);
+/**
+*@brief destructor 
+*/	
 	virtual ~TankDriver();
-	Vehicle *tank;
+	Vehicle *tank; // stores the tank that the driver drives
+/**
+*@brief virtual getter function to return the damage member variable
+*@return int
+*/	
 	virtual int getDamage();
 };
 #endif
