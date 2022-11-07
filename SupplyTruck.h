b0d322a2936e599b04465e7ac0e64f1b3fe2f74b
emileepeyton <u21494577@tuks.co.za>
2022-11-07 14:11:44 +0200

    supplyTruck doxygen

--- SupplyTruck.h
+++ SupplyTruck.h
@@ -1,9 +1,24 @@
+/**
+ * @file SupplyTruc.h
+ * @author Emilee da Cruz
+ * @brief Header file for SupplyTruck class which inherits from Vehicle class
+ * @date 2022-11-06
+ */
 #ifndef SUPPLYTRUCK_H
 #define SUPPLYTRUCK_H
 #include "Vehicle.h"
+/**
+*@class SupplyTruck
+*/
 class SupplyTruck : public Vehicle{
     public: 
+    /**
+    *@brief SupplyTruck object constructor
+    */
     SupplyTruck();
+    /**
+    *@brief destructor destroys supply truck object
+    */
     virtual ~SupplyTruck();
 };
 #endif
