54a59779d81937c357ce2f2ae688b3bfacaac519
emileepeyton <u21494577@tuks.co.za>
2022-11-07 14:17:09 +0200

    stealth doxygen

--- Stealth.h
+++ Stealth.h
@@ -1,9 +1,25 @@
+/**
+ * @file Stealth.h
+ * @author Emilee da Cruz
+ * @brief Header file for Stealth class which decorated Infantry object
+ * @date 2022-11-06
+ */
 #ifndef STEALTH_H
 #define STEALTH_H
 #include "Infantry.h"
+/**
+*@class Stealth
+*/
 class Stealth { 
     public: 
+    /**
+    *@brief constructor for Stealth object
+    *@param Infantry*
+    */
     Stealth(Infantry*); 
+    /**
+    *@brief destroys stealth object
+    */
     ~Stealth();
 };
 #endif
