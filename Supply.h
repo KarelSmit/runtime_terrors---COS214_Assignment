eada8def30e3b615880919fecf3ecaf2473bc910
emileepeyton <u21494577@tuks.co.za>
2022-11-07 14:15:10 +0200

    supply doxygen

--- Supply.h
+++ Supply.h
@@ -1,13 +1,32 @@
+/**
+ * @file Tank.h
+ * @author Emilee da Cruz
+ * @brief Header file for Supply class which inherits from TransportUnit class
+ * @date 2022-11-06
+ */
 #ifndef SUPPLY_H
 #define SUPPLY_H
 #include "TransportUnit.h"
 #include "Vehicle.h"
+/**
+*@class Supply
+*/
 class Supply : public TransportUnit {
     private:
-    Vehicle* supplyTruck;
+    Vehicle* supplyTruck; // member variable holds a supplyTruck object in a vehicle pointer
     public: 
+    /**
+    *@brief constructor for supply 
+    */
     Supply(int); 
+    /**
+    *@brief destroys supply object
+    */
     virtual ~Supply();
+    /**
+    *@brief Getter function to return the object as a Vehicle*
+    *@return Vehicle*
+    */
     Vehicle* getSupplyTruck();
 }; 
 #endif
