fece1da5f978c2fbff232eaae04cba862cd103f4
emileepeyton <u21494577@tuks.co.za>
2022-11-07 02:17:54 +0200

    transport unit doxygen

--- TransportUnit.h
+++ TransportUnit.h
@@ -1,15 +1,37 @@
-
+/**
+ * @file TransportUnit.h
+ * @author Emilee da Cruz
+ * @brief Transport unit class header file
+ * @date 2022-11-06
+ */
 #ifndef TRANSPORTUNIT_H
 #define TRANSPORTUNIT_H
+/**
+*@class TransportUnit
+*/
 class TransportUnit
 {
 protected:
 	int HP;
 
 public:
+/**
+*@brief constructor for transport unit
+*/
 	TransportUnit(int);
+/**
+*@brief destructor for transport unit
+*/	
 	virtual ~TransportUnit();
+/**
+*@brief getter function to return the HP member variable
+*@return int
+*/	
 	int getHP();
+/**
+*@brief virtual function for returning the damage member variable of a class
+*@return int
+*/	
 	virtual int getDamage();
 };
 #endif
