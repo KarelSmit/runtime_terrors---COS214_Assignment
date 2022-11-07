f7dd76777b668f3cfc0555f78892137371055d3e
emileepeyton <u21494577@tuks.co.za>
2022-11-07 02:20:08 +0200

    TankFactory doxygen

--- TankFactory.h
+++ TankFactory.h
@@ -1,10 +1,29 @@
+/**
+ * @file TankFactory.h
+ * @author Emilee da Cruz
+ * @brief Inherits from the Vehicle factory and is used to make objects of type Tank
+ * @date 2022-11-06
+ */
 #ifndef TANKFACTORY_H
 #define TANKFACTORY_H
 #include "VehicleFactory.h"
+/**
+*@class TankFactory
+*/
 class TankFactory : public VehicleFactory {
     public: 
+    /**
+    *@brief constructor
+    */
     TankFactory(); 
-    Vehicle* createVehicles(); 
+    /**
+    *@brief function for creating tank objects
+    *@return Vehicle*
+    */
+    Vehicle* createVehicles();
+    /**
+    *@brief virtual destructor
+    */ 
     virtual ~TankFactory(); 
 };
 #endif
