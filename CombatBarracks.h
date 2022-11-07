a13158a7555cd3dab3f49e09d306ba3b8e2a0c42
emileepeyton <u21494577@tuks.co.za>
2022-11-07 14:35:04 +0200

    combatBarracks doxygen

--- CombatBarracks.h
+++ CombatBarracks.h
@@ -1,12 +1,35 @@
+/**
+ * @file CombatBarracks.h
+ * @author Emilee da Cruz
+ * @brief Header file for CombatBarracks class which inherits from Barracks class
+ * @date 2022-11-06
+ */
 #ifndef COMBATBARRACKS_H
 #define COMBATBARRACKS_H
 #include "Barracks.h"
+/**
+*@class CombatBarracks
+*/
 class CombatBarracks : public Barracks
 {
 public:
+/**
+*@brief constructs a combatbarracks object
+*/
 	CombatBarracks();
+	/**
+	*@brief creates a foot unit object 
+	*@return footUnit*
+	*/
 	FootUnit *createFootUnit();
+	/**
+	*@brief creates a transportUnit object
+	*@return TransportUnit*
+	*/
 	TransportUnit *createTransportUnit();
+	/**
+	*@brief destroys combatBarracks object
+	*/
 	virtual ~CombatBarracks();
 };
 #endif
