10074543c338e545a40e4e5386204913bc906b6b
emileepeyton <u21494577@tuks.co.za>
2022-11-07 02:35:55 +0200

    supportBarracks doxygen

--- SupportBarracks.h
+++ SupportBarracks.h
@@ -1,10 +1,30 @@
+/**
+ * @file SupportBarracks.h
+ * @author Emilee da Cruz
+ * @brief Inherits from Barracks and is used for creating support barracks
+ * @date 2022-11-06
+ */
 #ifndef SUPPORTBARRACKS_H
 #define SUPPORTBARRACKS_H
 #include "Barracks.h"
+/**
+*@class SupportBarracks
+*/
 class SupportBarracks : public Barracks {
     public:
+/**
+*@brief default constructor
+*/
     SupportBarracks(); 
-    FootUnit* createFootUnit(); 
+/**
+*@brief function for creating foot unit soldiers
+*@return FootUnit*
+*/    
+    FootUnit* createFootUnit();
+/**
+*@brief class for creating transport unit soldiers
+*@return TransportUnit*
+*/     
     TransportUnit* createTransportUnit();
 }; 
 #endif
