4c77c48521942a6655675baa0aa4cb81b9dcb21a
emileepeyton <u21494577@tuks.co.za>
2022-11-07 14:20:31 +0200

    special skill doxygen

--- SpecialSkill.h
+++ SpecialSkill.h
@@ -1,13 +1,33 @@
+/**
+ * @file SpecialSkill.h
+ * @author Emilee da Cruz
+ * @brief Header file for SpeicalSkill class which acts as the decorator 
+ * @date 2022-11-06
+ */
 #ifndef SPECIALSKILL_H
 #define SPECIALSKILL_H
 #include "Infantry.h"
+/**
+*@class SpecialSkill
+*/
 class SpecialSkill {
     private: 
-    Infantry* soldier;
-    int choice;
+    Infantry* soldier; // member variable to hold an infantry object
+    int choice; // member variable for deciding whether to call stealth or sniper class
     public: 
-    void addSkill(); 
+    /**
+    *@brief function to add a special skill to an infantry object
+    *@return void
+    */
+    void addSkill();
+    /**
+    *@brief Constructor for special skill
+    *@param Infantry* , int
+    */ 
     SpecialSkill(Infantry*, int);
+    /**
+    *@brief destroys special skill object 
+    */
     ~SpecialSkill();
 }; 
 #endif 
